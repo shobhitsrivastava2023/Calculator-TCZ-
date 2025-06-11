daedb4393de964fa4049f3304a5bb70c96f34a02
shobhitsrivastava2023 <shobhit.229302512@muj.manipal.edu>
2025-06-11 11:53:27 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const display = document.getElementById('display');
-
+const keys = ['0','1','2','3','4','5','6','7','8','9','+', '-', '*', '/', '.'];
 
 function appendToDisplay(value) { 
   display.value += value; 
@@ -18,3 +18,25 @@ function calculate() {
     display.value = 'Error'; 
   }
 }
+
+
+
+
+document.addEventListener('keydown', function(event) { 
+  const key = event.key; 
+  if (key === 'Enter' || key === '='){ 
+    calculate(); 
+    event.preventDefault();
+  }
+  if (keys.includes(key)) { 
+    this.appendToDisplay(key);
+  event.preventDefault(); 
+  }
+  if(key === 'Backspace'){ 
+    display.value = display.value.slice(0, -1); 
+    event.preventDefault(); 
+  }
+  else { 
+    event.preventDefault(); 
+  }
+})
